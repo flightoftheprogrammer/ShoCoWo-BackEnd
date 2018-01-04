3654fb4d136e684fdbc7953918089b0d7a5778e3
Jeremy Warren <jeremyawarren15@gmail.com>
2018-01-04 10:58:38 -0500

    Added a Create method for WalletService and matching model

---
+++ ShoCoWo.Models/Wallet/WalletCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoCoWo.Models.Wallet
+{
+    public class WalletCreate
+    {
+        [DefaultValue(0.00)]
+        public decimal WalletBalance { get; set; }
+    }
+}
---
+++ ShoCoWo.Services/WalletService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoCoWo.Data;
+using ShoCoWo.Models.Wallet;
+
+namespace ShoCoWo.Services
+{
+    public class WalletService
+    {
+        private readonly Guid _userId;
+
+        public WalletService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateWallet(WalletCreate model)
+        {
+            var entity =
+                new Wallet()
+                {
+                    UserId = _userId,
+                    WalletBalance = model.WalletBalance,
+                    CreatedUtc = DateTimeOffset.UtcNow
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Wallets.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
