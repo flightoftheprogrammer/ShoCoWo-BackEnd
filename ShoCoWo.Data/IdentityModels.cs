1bf86758af6d94c64aa399519f723d099dd8dd0e
Jeremy Warren <jeremyawarren15@gmail.com>
2018-01-03 21:28:17 -0500

    Added IdentityUserLoginConfiguration to IdentityModels

--- ShoCoWo.Data/IdentityModels.cs
+++ ShoCoWo.Data/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity.ModelConfiguration;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -30,4 +31,14 @@ namespace ShoCoWo.Data
             return new ApplicationDbContext();
         }
     }
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+
 }
