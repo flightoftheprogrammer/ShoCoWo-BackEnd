c327ed67d3dc064a73fce40f53834e5b0791950b
Jeremy Warren <jeremyawarren15@gmail.com>
2018-01-09 09:56:20 -0500

    added a method to create a holding service in the controller

---
+++ ShoCoWo.Api/Controllers/HoldingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using ShoCoWo.Services;
+
+namespace ShoCoWo.Api.Controllers
+{
+    public class HoldingController : ApiController
+    {
+        private HoldingService CreateHoldingService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new HoldingService(userId);
+            return service;
+        }
+    }
+}
