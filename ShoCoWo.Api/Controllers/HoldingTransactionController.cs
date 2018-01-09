6962d06b2d212020ee73006d394c7c413ad4f95f
Jeremy Warren <jeremyawarren15@gmail.com>
2018-01-09 10:49:28 -0500

    refactored code to fix HoldingTransactionController

---
+++ ShoCoWo.Api/Controllers/HoldingTransactionController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using ShoCoWo.Data;
+using ShoCoWo.Models.HoldingTransaction;
+using ShoCoWo.Models.WalletTransaction;
+using ShoCoWo.Services;
+
+namespace ShoCoWo.Api.Controllers
+{
+    public class HoldingTransactionController : ApiController
+    {
+        private HoldingTransactionService CreateHoldingTransactionService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new HoldingTransactionService(userId);
+            return service;
+        }
+
+        public IHttpActionResult Post(HoldingTransactionCreate model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateHoldingTransactionService();
+
+            if (!service.CreateHoldingTransaction(model))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult GetHoldingTransactions(int id)
+        {
+            var service = CreateHoldingTransactionService();
+            var transactions = service.GetHoldingTransactions(id);
+
+            return Ok(transactions);
+        }
+
+        public IHttpActionResult GetHoldingTransaction(int transactionId)
+        {
+            var service = CreateHoldingTransactionService();
+            var transaction = service.GetHoldingTransaction(transactionId);
+
+            return Ok(transaction);
+        }
+    }
+}
