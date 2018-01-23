2f9d90856919e115e9c6bfb937466deeae0f3a47
Jeremy Warren <jeremyawarren15@gmail.com>
2018-01-22 20:26:44 -0500

    added configuration to add an admin if there is none

--- ShoCoWo.Api/Startup.cs
+++ ShoCoWo.Api/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Owin;
+using ShoCoWo.Data;
 
 [assembly: OwinStartup(typeof(ShoCoWo.Api.Startup))]
 
@@ -14,6 +17,34 @@ namespace ShoCoWo.Api
         {
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        private void CreateRolesAndUsers()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(ctx));
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(ctx));
+
+                if (!roleManager.RoleExists("admin"))
+                {
+                    var role = new IdentityRole();
+                    role.Name = "admin";
+                    roleManager.Create(role);
+
+                    var user = new ApplicationUser();
+                    user.UserName = "admin";
+                    user.Email = "admin@admin.com";
+
+                    string userPassword = "Test1!";
+
+                    var chkUser = userManager.Create(user, userPassword);
+
+                    if (chkUser.Succeeded)
+                        userManager.AddToRole(user.Id, "admin");
+                }
+            }
         }
     }
 }
